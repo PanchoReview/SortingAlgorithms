ba14f477ff4d0e85cc73b2a67c22b8e7182651ec
Francisco Astorga <fastorga@nimok.com>
2021-12-25 18:28:31 -0300

    Implemented InsertionSort
    Signed-off-by: Francisco Astorga <fastorga@nimok.com>

---
+++ SortingAlgorithms/InsertionSort.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithms
+{
+    public class InsertionSort
+    {
+        //Walk through an unstorted array from the first index onwards
+        //Compare the value of current index to those before it, and swap each value depending on their values
+        //Time complexity of this algorithm is (N*N) (N squared)
+        public static void Execute(int[] array) 
+        {
+            int arrayLength = array.Length;
+            if (arrayLength < 2)
+                return;
+
+            //We start walking through the array from left to right, starting with the second position at index 1
+            //This is because element at index 0 is already sorted in itself, and there are no further elements to compare to it's left
+            for (int i = 1; i < arrayLength; i++)
+            {   
+                int currentValue = array[i]; //We store the current value in a variable
+                int j = i -1; //We set j as our backwards iterator, which we'll use to inspect elements to the left of our current value
+
+                //while j is greater than zero AND the element at position j is greater than the current value
+                //we swap the position of currentValue and that of j
+                while(j >= 0 && array[j] > currentValue)
+                {
+                    array[j + 1] = array[j];
+                    j--;
+                }
+
+                //At the end of the loop, we set the position of the current value as j + 1
+                //If there were no iterations in the previous while, the position of current value will remain the same
+                array[j + 1] = currentValue;
+            }
+        }
+
+    }
+}
--- SortingAlgorithms/Program.cs
+++ SortingAlgorithms/Program.cs
@@ -12,14 +12,35 @@ namespace SortingAlgorithms
             LogQuicksort(new int[] { 8, 9, 10, -5, -4, -2, 7, 15, 1080, 208, 597, 475, 784, 39 });
             LogQuicksort(new int[] { -854, 1583, -12, 987, 1, 2, 3, 3, 4, 5879, 1897585, 8, 8, 9, 10, -5, -4, -2, 7, 15, 1080, 208, 597, 475, 784, 39 });
 
+            Console.WriteLine("");
 
             LogMergeSort(new int[] { 4, 9, 2, 1, 6, 3, 8 });
             LogMergeSort(new int[] { 8, 9, 10, -5, -4, -2, 7, 15, 1080, 208, 597, 475, 784, 39 });
             LogMergeSort(new int[] { -854, 1583, -12, 987, 1, 2, 3, 3, 4, 5879, 1897585, 8, 8, 9, 10, -5, -4, -2, 7, 15, 1080, 208, 597, 475, 784, 39 });
 
+            Console.WriteLine("");
+
             LogBubbleSort(new int[] { 4, 9, 2, 1, 6, 3, 8 });
             LogBubbleSort(new int[] { 8, 9, 10, -5, -4, -2, 7, 15, 1080, 208, 597, 475, 784, 39 });
             LogBubbleSort(new int[] { -854, 1583, -12, 987, 1, 2, 3, 3, 4, 5879, 1897585, 8, 8, 9, 10, -5, -4, -2, 7, 15, 1080, 208, 597, 475, 784, 39 });
+
+            Console.WriteLine("");
+
+            LogInsertionSort(new int[] { 4, 9, 2, 1, 6, 3, 8 });
+            LogInsertionSort(new int[] { 8, 9, 10, -5, -4, -2, 7, 15, 1080, 208, 597, 475, 784, 39 });
+            LogInsertionSort(new int[] { -854, 1583, -12, 987, 1, 2, 3, 3, 4, 5879, 1897585, 8, 8, 9, 10, -5, -4, -2, 7, 15, 1080, 208, 597, 475, 784, 39 });
+        }
+
+        private static void LogInsertionSort(int[] list)
+        {
+            InsertionSort.Execute(list);
+
+            string array = "";
+
+            foreach (var item in list)
+                array = $"{array} {item}";
+
+            Console.WriteLine($"Insertion Sort: {array}");
         }
 
         private static void LogBubbleSort(int[] list)
